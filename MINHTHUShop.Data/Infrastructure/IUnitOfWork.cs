2c568320c3344fa75379b84bbc3f60e477d6ad85
NguyenNgocHongHan <han.nnh.work@gmail.com>
2023-04-17 19:03:50 +0700

    Add infrastructure for Data

---
+++ MINHTHUShop.Data/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace MINHTHUShop.Data.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        //lưu trữ các thay đổi vào cơ sở dữ liệu
+        void Commit();
+    }
+}
---
+++ MINHTHUShop.Data/MINHTHUShopDbContext.cs
@@ -0,0 +1,53 @@
+﻿using MINHTHUShop.Model.Abstract;
+using MINHTHUShop.Model.Models;
+using System.Data.Entity;
+
+namespace MINHTHUShop.Data
+{
+    public class MINHTHUShopDbContext : DbContext
+    {
+        public MINHTHUShopDbContext() : base("MINHTHUShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false; //khi load bảng cha sẽ không tự động includes bảng con
+        }
+
+        public DbSet<Tb_About> Tb_Abouts { get; set; }
+        public DbSet<Tb_Banner> Tb_Banners { get; set; }
+        public DbSet<Tb_Brand> Tb_Brands { get; set; }
+        public DbSet<Tb_Customer> Tb_Customers { get; set; }
+        public DbSet<Tb_FAQ> Tb_FAQs { get; set; }
+        public DbSet<Tb_FAQCategory> Tb_FAQCategories { get; set; }
+        public DbSet<Tb_Feedback> Tb_Feedbacks { get; set; }
+        public DbSet<Tb_Menu> Tb_Menus { get; set; }
+        public DbSet<Tb_MenuGroup> Tb_MenuGroups { get; set; }
+        public DbSet<Tb_News> Tb_News { get; set; }
+        public DbSet<Tb_NewsCategory> Tb_NewsCategories { get; set; }
+        public DbSet<Tb_Order> Tb_Orders { get; set; }
+        public DbSet<Tb_OrderDetail> Tb_OrderDetails { get; set; }
+        public DbSet<Tb_OrderStatus> Tb_OrderStatuses { get; set; }
+        public DbSet<Tb_Payment> Tb_Payments { get; set; }
+        public DbSet<Tb_PaymentMethod> Tb_PaymentMethods { get; set; }
+        public DbSet<Tb_Product> Tb_Products { get; set; }
+        public DbSet<Tb_ProductCategory> Tb_ProductCategories { get; set; }
+        public DbSet<Tb_ProductComment> Tb_ProductComments { get; set; }
+        public DbSet<Tb_Role> Tb_Roles { get; set; }
+        public DbSet<Tb_Shipping> Tb_Shippings { get; set; }
+        public DbSet<Tb_ShippingMethod> Tb_ShippingMethods { get; set; }
+        public DbSet<Tb_Staff> Tb_Staffs { get; set; }
+        public DbSet<Tb_Tag> Tb_Tags { get; set; }
+        public DbSet<Tb_TagNews> Tb_TagNews { get; set; }
+        public DbSet<Tb_TagProduct> Tb_TagProducts { get; set; }
+        public DbSet<Tb_Target> Tb_Targets { get; set; }
+
+        //phương thức ghi đè DbContext
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            /*
+            modelBuilder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId }).ToTable("ApplicationUserRoles");
+            modelBuilder.Entity<IdentityUserLogin>().HasKey(i => i.UserId).ToTable("ApplicationUserLogins");
+            modelBuilder.Entity<IdentityRole>().ToTable("ApplicationRoles");
+            modelBuilder.Entity<IdentityUserClaim>().HasKey(i => i.UserId).ToTable("ApplicationUserClaims");
+            */
+        }
+    }
+}
