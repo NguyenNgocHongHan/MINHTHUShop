934b419ae658167f0959f15cc4efee0ea64da251
NguyenNgocHongHan <han.nnh.work@gmail.com>
2023-05-02 13:46:29 +0700

    Add bootstrap and jquery format files

--- MINHTHUShop.Web/App_Start/Startup.cs
+++ MINHTHUShop.Web/App_Start/Startup.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.Owin;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using MINHTHUShop.Data;
+using MINHTHUShop.Data.Infrastructure;
+using MINHTHUShop.Data.Repositories;
+using MINHTHUShop.Service;
 using Owin;
-using System;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
 
 [assembly: OwinStartup(typeof(MINHTHUShop.Web.App_Start.Startup))]
 
@@ -12,6 +20,38 @@ namespace MINHTHUShop.Web.App_Start
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+            //ConfigureAuth(app);
+        }
+
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //đăng ký WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<MINHTHUShopDbContext>().AsSelf().InstancePerRequest();
+
+            //Asp.net Identity
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(Tb_NewsCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(Tb_NewsCategoryService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
         }
     }
 }
