f574d3de70925d3a58ef0266a93710d0ce2c75c6
NguyenNgocHongHan <han.nnh.work@gmail.com>
2023-05-08 14:08:40 +0700

    Add filter configuration file for tables with Status attribute

---
+++ MINHTHUShop.Web/app/shared/filter/statusFillter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == 1)
+                return 'Đang hoạt động';
+            else
+                return 'Ngừng hoạt động';
+        }
+    });
+})(angular.module('MINHTHUShop.common'));
